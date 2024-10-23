5d92d5ec8b9c96962486bd18ebe0dbb203db4ff6
Andy Baizon <dev@baizon.uk>
2024-10-23 22:05:41 +0100

    feat(player): basic player rendering & movement

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 import pygame
 from constants import *
+from player import *
 
 
 def main():
     pygame.init()
-    # print("Starting asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    framerate = 100
+    dt = 0
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2), PLAYER_RADIUS)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
+
+        for item in updateable:
+            item.update(dt)
+
+        screen.fill("black")
+
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
+
+        # limit framerate
+        dt = clock.tick(framerate) / 1000
 
 
 if __name__ == "__main__":
